2cce4ebd66beda2a8cdae1a6e0bfb90d40663500
Kartik Pawar <knpawar789@gmail.com>
2025-05-08 01:30:59 +0530

    fix: analytics import react -> next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { DATA } from "@/data/resume";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const fontSans = FontSans({

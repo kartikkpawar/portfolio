e7e18839c2c3f63d2ccb8d662061bac9dd4da5c2
Kartik Pawar <knpawar789@gmail.com>
2025-05-09 13:50:34 +0530

    feat: fe-resume added

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,12 @@ const nextConfig = {
           "https://drive.google.com/file/d/1elzbunB8PFI6MV5zUJWXba1sV3_Hrd1G/view?usp=drive_link",
         permanent: true,
       },
+      {
+        source: "/fe-resume",
+        destination:
+          "https://drive.google.com/file/d/1p5EiMmBzp2fbrSOlPXeFvgA-3GCIMeGI/view?usp=drive_link",
+        permanent: true,
+      },
     ];
   },
 };
